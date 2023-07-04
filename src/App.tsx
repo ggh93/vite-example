7dfe97f595106c8462074bbd01c1aa4a339dd8f6
ggh93 <ggh93@naver.com>
2023-07-04 19:01:45 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="main">
+        <h2>로그인 페이지</h2>
+        <div className="container">
+          <input type="text" className="account" placeholder="ID" />
+          <input type="text" className="account" placeholder="PASSWORD" />
+          <button className="login" onClick={() => []}>
+            Login
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
